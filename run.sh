165e23a351cccffe3627c00d7fe1d4e58ce9eb0f
Robert Bendun <rob.bendun@gmail.com>
2023-05-16 19:47:48 +0200

    Hoist configuration to remote variables

--- run.sh
+++ run.sh
@@ -2,12 +2,18 @@
 
 set -xe
 
+# TODO: Move to .env file
+REMOTE_NETWORK="192.168.122.0/24"
+REMOTE_USER="lambda"
+REMOTE_USER_BECOME_PASSWORD="ensamble"
+
+# TODO: This cache should have been overwritten via a flag like --scan
 if [ ! -f hosts ]; then
 	echo "[lambda_ensamble_vm]" > hosts
-	nmap -n -sn 192.168.122.0/24 -oG - | awk '/Up$/{print $2}' | grep -vE "^$(hostname -i | tr ' ' '|')\$" >> hosts
+	nmap -n -sn "${REMOTE_NETWORK}" -oG - | awk '/Up$/{print $2}' | grep -vE "^$(hostname -i | tr ' ' '|')\$" >> hosts
 	echo "[lambda_ensamble_vm:vars]" >> hosts
-	echo "ansible_user=lambda" >> hosts
-	echo "ansible_become_password=ensamble" >> hosts
+	echo "ansible_user=${REMOTE_USER}" >> hosts
+	echo "ansible_become_password=${REMOTE_USER_BECOME_PASSWORD}" >> hosts
 fi
 
 ANSIBLE_NOCOWS=1 ansible-playbook -i hosts configure.yml -v
