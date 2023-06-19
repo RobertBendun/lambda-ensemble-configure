536c8a5bd0fdd39612de61ede440a78bf093b56a
Robert Bendun <robert@bendun.cc>
2023-06-19 23:12:04 +0200

    avahi

--- generate-hosts.sh
+++ generate-hosts.sh
@@ -3,14 +3,13 @@
 set -xe
 
 # TODO: Move to .env file
-REMOTE_NETWORK="192.168.122.0/24"
+REMOTE_NETWORK="192.168.1.0/24"
 REMOTE_USER="lambda"
-REMOTE_USER_BECOME_PASSWORD="ensamble"
+REMOTE_USER_BECOME_PASSWORD="12341234"
 
 echo "[lambda_ensamble_vm]" > hosts
 nmap -n -sn "${REMOTE_NETWORK}" -oG - | awk '/Up$/{print $2}' | grep -vE "^($(hostname -i | tr ' ' '|'))\$" >> hosts
 echo "[lambda_ensamble_vm:vars]" >> hosts
 echo "ansible_user=${REMOTE_USER}" >> hosts
 echo "ansible_become_password=${REMOTE_USER_BECOME_PASSWORD}" >> hosts
-
 
